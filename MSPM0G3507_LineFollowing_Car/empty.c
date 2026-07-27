9ff4941d8b629d045745e331909d56e6fceb173e
Reese <3297536247@qq.com>
2026-07-27 19:27:49 +0800

    debug: mark task creation checkpoints

--- MSPM0G3507_LineFollowing_Car/empty.c
+++ MSPM0G3507_LineFollowing_Car/empty.c
@@ -1,19 +1,32 @@
 #include "ti_msp_dl_config.h"
-#include "application/app_main.h"
-#include "application/freertos/app_tasks.h"
+#include "app/boot/app_boot.h"
+#include "app/tasks/app_tasks.h"
 #include "FreeRTOS.h"
 #include "task.h"
-#include "motor_safety.h"
+#include "modules/motor/safety/motor_safety.h"
+#include "modules/display/runtime_log.h"
+#include "modules/display/ssd1306/ssd1306.h"
+#include "modules/time/timer.h"
+
+static void BootLog(const char *event)
+{
+    (void)RuntimeLog_Push(Get_Time(), event);
+    RuntimeLog_Draw();
+    (void)Ssd1306_FlushDirty();
+}
 
 int main(void)
 {
     SYSCFG_DL_init();
-    App_Main_Init();
+    AppBoot_Init();
+    BootLog("CREATE TASK");
 
     if (!AppTasks_Create()) {
+        BootLog("TASK FAIL");
         Motor_Safety_Disarm();
         for (;;) {}
     }
+    BootLog("TASKS OK");
 
     vTaskStartScheduler();
     Motor_Safety_Disarm();
